7e30af436abf0b130fdd481275cdeb4232ca21c1
Brent Neal <bneal5@twc.com>
2025-12-09 11:49:25 -0500

    Add corrected project structure files

---
+++ Migrations/20251025202602_InitialBaseline.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialBaseline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Intentionally left empty to baseline existing database schema
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Intentionally left empty as rollback not needed for baseline
+        }
+    }
+}
